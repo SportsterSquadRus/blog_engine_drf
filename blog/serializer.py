051df91830489d84350ae5deef1b5c6e960880b2
suren <elek34ka@gmail.com>
2020-11-12 23:28:04 +0300

    author field changed

--- blog/serializer.py
+++ blog/serializer.py
@@ -4,12 +4,16 @@ from .models import Post
 
 class PostListSerializer(serializers.ModelSerializer):
 
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
         model = Post
         fields = ('author', 'title', 'body',)
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         model = Post
