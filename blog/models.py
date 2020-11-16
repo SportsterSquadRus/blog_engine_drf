364b5812b3f03692c70e8d5d37a65e4b6c1cc5ad
suren <elek34ka@gmail.com>
2020-11-16 18:41:17 +0300

    comment model and serializer

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,34 @@
 from django.db import models
 from django.contrib import auth
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        auth.models.User, related_name='comment', on_delete=models.CASCADE, null=True)
+    body = models.TextField(verbose_name='Текст комментярия')
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок поста')
     body = models.TextField(verbose_name='Текст поста')
-    published = models.DateTimeField(null=True, blank=True, verbose_name='Дата публикации')
+    published = models.DateTimeField(
+        null=True, blank=True, verbose_name='Дата публикации')
     truncate = models.IntegerField(default=0)
-    author = models.ForeignKey('auth.User', blank=True, null=True, on_delete=models.CASCADE, verbose_name='Автор')
+    author = models.ForeignKey(auth.models.User, blank=True,
+                               null=True, on_delete=models.CASCADE, verbose_name='Автор')
     draft = models.BooleanField(verbose_name='Черновик', default=False)
+    comments = GenericRelation(Comment)
+
+    def __str__(self):
+        return str(self.title)
+
 
 # {"title": "post177", "body": "lala<cut>lolo", "author":1}
-
--- blog/serializer.py
+++ blog/serializer.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'body', 'date_pub')
+
 
 
 class PostListSerializer(serializers.ModelSerializer):
@@ -12,6 +21,10 @@ class PostListSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    comments = CommentSerializer(many=True)
+
 
     class Meta:
         model = Post
